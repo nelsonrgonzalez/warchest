6418ff6b3c4dba90f1584f5954ac54ddb3826d88
nelsonrgonzalez <nrg2001@gmail.com>
2018-01-20 19:56:19 -0300

    Update feat_sel_sbs.py

--- feat_sel_sbs.py
+++ feat_sel_sbs.py
@@ -1,22 +1,3 @@
-"""
-    Warchest: Data management and automation GUI for Machine Learning projects
-    Created September 2017
-    Copyright (C) Nelson R Gonzalez
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
 from sklearn.base import clone
 from itertools import combinations
 import numpy as np
